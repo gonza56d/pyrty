72753ad7395a1781afd09ddf460d584e268467ff
Gonzalo D'ambra <gonza56d@gmail.com>
2020-08-09 01:58:07 -0300

    Create post.

--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,7 @@ import pdb
 
 # Django
 from django import forms
+from django.shortcuts import redirect
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 
@@ -41,8 +42,38 @@ class CreatePostView(CreateView):
 
 	fields = ['subforum', 'title', 'content']
 
-	#  TODO
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['subforum'] = self.subforum
+		return context
+
+	def get_form(self, form_class=None):
+		"""Init form by hiding subforum option and leaving current subforum as the only choice."""
+
+		form = super(CreatePostView, self).get_form(form_class)
+
+		form.fields['subforum'] = forms.ModelChoiceField(
+			queryset=Subforum.objects.filter(pk=self.subforum.id),
+			empty_label=None,
+			widget = forms.Select(attrs={'class': 'form-control', 'style': 'display:none;'})
+		)
+		form.fields['title'].widget = forms.TextInput(
+			attrs={'class': 'form-control my-2', 'placeholder': 'Title'}
+		)
+		form.fields['content'].widget = forms.Textarea(
+			attrs={'class': 'form-control my-2', 'placeholder': 'Content'}
+		)
+		return form
 
 	def get(self, request, *args, **kwargs):
 		self.subforum = Subforum.objects.get(pk=self.kwargs['subforum_id'])
 		return super().get(request, *args, **kwargs)
+
+	def post(self, request, *args, **kwargs):
+		self.subforum = Subforum.objects.get(pk=self.kwargs['subforum_id'])
+		form = self.get_form()
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.user = request.user
+			post.save()
+			return redirect('post', pk=post.id)
