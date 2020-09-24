4091924c022835ae136ea1f83ec4a6a31276211d
Gonzalo D'ambra <gonza56d@gmail.com>
2020-09-23 21:13:24 -0300

    PrivateMessage REST list and retrieve

--- privatemessages/urls.py
+++ privatemessages/urls.py
@@ -1,13 +1,21 @@
 """PrivateMessage urls."""
 
 # Django
-from django.urls import path
+from django.urls import include, path
+
+# Django REST Framework
+from rest_framework import routers
 
 # Pyrty
+from privatemessages.rest.views import PrivateMessageViewSet
 from privatemessages.views import PrivateMessageList, create_private_message
 
 
+router = routers.SimpleRouter()
+router.register(r'privatemessages', PrivateMessageViewSet, basename='privatemessages')
+
 urlpatterns = [
 	path('inbox/<str:origin_user>', PrivateMessageList.as_view(), name='private_message'),
-	path('create_private_message/', create_private_message, name='create_private_message')
+	path('create_private_message/', create_private_message, name='create_private_message'),
+	path('rest/'. include(router.urls))
 ]
