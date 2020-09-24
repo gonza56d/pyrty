d5508e8cd870185481e0d32f37e826b932058ca9
Gonzalo D'ambra <gonza56d@gmail.com>
2020-09-23 21:11:20 -0300

    PrivateMessage REST list and retrieve

--- privatemessages/rest/views.py
+++ privatemessages/rest/views.py
@@ -1,9 +1,15 @@
 """PrivateMessage REST views."""
 
+# Django
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
+
 # Django REST Framework
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 # Pyrty
+from privatemessages.models import PrivateMessage
 from privatemessages.rest.serializers import PrivateMessageSerializer
 
 
@@ -11,4 +17,19 @@ class PrivateMessageViewSet(viewsets.ModelViewSet):
 
 	serializer_class = PrivateMessageSerializer
 
-	# TODO
+	def list(self, request):
+		queryset = PrivateMessage.objects.filter(
+			Q(origin_user=request.user) |
+			Q(target_user=request.user)
+		)
+		serializer = PrivateMessageSerializer(queryset, many=True)
+		return Response(serializer.data)
+
+	def retrieve(self, request, pk=None):
+		queryset = PrivateMessage.objects.filter(
+			Q(origin_user=request.user) |
+			Q(target_user=request.user)
+		)
+		private_message = get_object_or_404(queryset, pk=pk)
+		serializer = PrivateMessageSerializer(queryset)
+		return Response(serializer.data)
