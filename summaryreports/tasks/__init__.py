dd4363caad6b97986d5432d3a310f24b582e10e3
Gonza <gonza56d@gmail.com>
2020-10-29 22:21:26 -0300

    Import fix

--- summaryreports/tasks/__init__.py
+++ summaryreports/tasks/__init__.py
@@ -1,4 +1,4 @@
-from summaryreports.tasks.async import create_summary_report
+from summaryreports.tasks.asyncs import create_summary_report
 from summaryreports.tasks.periodic import (
         send_daily_summary_report, send_weekly_summary_report, send_monthly_summary_report
     )
