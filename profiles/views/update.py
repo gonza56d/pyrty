29a66f4a38e3f64b6f10b2469602af1d141ce36b
Gonza <gonza56d@gmail.com>
2020-11-07 03:37:07 -0300

    Fix translation on user update form

--- profiles/views/update.py
+++ profiles/views/update.py
@@ -7,6 +7,7 @@
 from django import forms
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 from django.views.generic.edit import UpdateView
 
 # Pyrty
@@ -19,63 +20,67 @@ from users.models import User
 
 
 class ProfileUpdateView(UpdateView):
-	"""Display own user's profile info and handle update."""
-
-	model = Profile
-	fields = ['first_name', 'last_name', 'birthday', 'bio']
-
-	def get_object(self, queryset=None):
-		"""Override to select related user."""
-
-		return Profile.objects.select_related('user').get(
+    """Display own user's profile info and handle update."""
+    
+    model = Profile
+    fields = ['first_name', 'last_name', 'birthday', 'bio']
+    
+    def get_object(self, queryset=None):
+        """Override to select related user."""
+        
+        return Profile.objects.select_related('user').get(
 			slug=self.kwargs.get(self.slug_url_kwarg)
-		)
-
-	def get_form(self, form_class=None):
-		"""Set widgets for form and initialy disable each field."""
-		
-		form = super(UpdateView, self).get_form(form_class)
-
-		form.fields['first_name'].widget = forms.TextInput(
+        )
+    
+    def get_form(self, form_class=None):
+        """Set widgets for form and initialy disable each field."""
+        
+        form = super(UpdateView, self).get_form(form_class)
+        
+        form.fields['first_name'].label = _('First name')
+        form.fields['first_name'].widget = forms.TextInput(
 			attrs={'class': 'form-control', 'disabled': ''}
 		)
-		form.fields['last_name'].widget = forms.TextInput(
+        form.fields['last_name'].label = _('Last name')
+        form.fields['last_name'].widget = forms.TextInput(
 			attrs={'class': 'form-control', 'disabled': ''}
 		)
-		form.fields['birthday'].widget = forms.DateInput(
+        form.fields['birthday'].label = _('Birthday')
+        form.fields['birthday'].widget = forms.DateInput(
 			attrs={'class': 'form-control', 'disabled': '', 'type': 'date'}
 		)
-		form.fields['bio'].widget = forms.Textarea(
+        form.fields['bio'].label = _('Bio')
+        form.fields['bio'].widget = forms.Textarea(
 			attrs={'class': 'form-control', 'disabled': '', 'rows': '5'}
 		)
-		return form
+        return form
+    
+    def get_success_url(self):
+        """Redirect to own profile on update success."""
+        return reverse('self_profile', args=[self.request.user])
+    
+    def get(self, request, *args, **kwargs):
+        """Redirect to ProfileDetailView if the requested profile is not request.user."""
+        user = None
+        try:
+            user = User.objects.get(username=self.kwargs['slug'])
+        finally:
+            if request.user != user:
+                return redirect('profile', slug=self.kwargs['slug'])
+            self.object = self.get_object()
+            return super().get(request, *args, **kwargs)
 
-	def get_success_url(self):
-		"""Redirect to own profile on update success."""
-		return reverse('self_profile', args=[self.request.user])
+    def get_context_data(self, **kwargs):
+        """Load profile's last posts and comments."""
+        context = super().get_context_data(**kwargs)
 
-	def get(self, request, *args, **kwargs):
-		"""Redirect to ProfileDetailView if the requested profile is not request.user."""
-		user = None
-		try:
-			user = User.objects.get(username=self.kwargs['slug'])
-		finally:
-			if request.user != user:
-				return redirect('profile', slug=self.kwargs['slug'])
-			self.object = self.get_object()
-			return super().get(request, *args, **kwargs)
+        user = self.object.user
 
-	def get_context_data(self, **kwargs):
-		"""Load profile's last posts and comments."""
-		context = super().get_context_data(**kwargs)
-
-		user = self.object.user
-
-		context['user_configurations_form'] = UserConfigurationsForm(
+        context['user_configurations_form'] = UserConfigurationsForm(
 			initial={'summary_reports': user.summary_reports}
-		)
-		context['profile_posts'] = Post.objects.select_related('subforum__forum')\
+        )
+        context['profile_posts'] = Post.objects.select_related('subforum__forum')\
 			.filter(user=user)[:10]
 
-		context['profile_comments'] = Comment.objects.filter(user=user)[:10]
-		return context
+        context['profile_comments'] = Comment.objects.filter(user=user)[:10]
+        return context
